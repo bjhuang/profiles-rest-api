597a090388abc19a56018051a0f9a673c9d76804
Bingjie Huang <bj_huang@hotmail.com>
2019-09-28 11:02:19 -0400

    git repository update

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -3,8 +3,7 @@
 set -e
 
 # TODO: Set to URL of git repo.
-PROJECT_GIT_URL='https://github.com/LondonAppDev/profiles-rest-api.git'
-
+PROJECT_GIT_URL=¡®git@github.com:bjhuang/profiles-rest-api.git¡¯
 PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
 
 echo "Installing dependencies..."
